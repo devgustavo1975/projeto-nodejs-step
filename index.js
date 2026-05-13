2ffa5ef383d1d7f6f041af792620e07e201e7e87
Gustavo Fernandes de Almeida <gustty75rj@hotmail.com>
2026-05-13 20:29:22 -0300

    aula 1

---
+++ index.js
@@ -0,0 +1,45 @@
+
+const http = require('http');
+
+const produtos = [
+  'Notebook',
+  'Mouse',
+  'Teclado',
+  'Monitor',
+  'Impressora',
+  'Cadeira Gamer',
+  'Mesa para Computador',
+  'Headset',
+];
+
+const server = http.createServer((req, res) => {
+  const listaProdutos = produtos
+    .map((produto) => `<li>${produto}</li>`)
+    .join('');
+
+  const html = `
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Gustavo Tecnologia</title>
+    </head>
+    <body>
+      <h1>Produtos</h1>
+
+      <ul>
+        ${listaProdutos}
+      </ul>
+    </body>
+    </html>
+  `;
+
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.end(html);
+});
+
+server.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000');
+});
+    
