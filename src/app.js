87004299099373f8c152204937f1c9cefad77e3b
Gustavo Fernandes de Almeida <gustty75rj@hotmail.com>
2026-06-08 19:57:12 -0300

    implementa upload e exportacao excel

--- src/app.js
+++ src/app.js
@@ -9,12 +9,16 @@ const produtoRoutes = require(
   './routes/produtoRoutes'
 );
 
+const arquivoRoutes = require(
+  './routes/arquivoRoutes');
+
 const app = express();
 
 app.use(express.json());
 
 app.use(usuarioRoutes);
 app.use(produtoRoutes);
+app.use(arquivoRoutes);
 
 module.exports = app;
             
